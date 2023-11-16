470d7985531a086096fad40d652f3a0496285dff
Milan <4k.milan15@gmail.com>
2023-11-16 12:07:47 +0500

    more_changes

--- pages/eom_2/javascript_quiz_app_10/scripts/script5.js
+++ pages/eom_2/javascript_quiz_app_10/scripts/script5.js
@@ -1,4 +1,4 @@
-const anwserArr = [['во время разработки и после нее создается документация и руководства пользователя'], ['процесс включает работу с компонентами программного обеспечения и управление их версиями'], ['необходим для проверки соответствия программного продукта требованиям разработки и стандартам'], ['позволяет независимо оценить проект, документацию и различные отчеты, сравнивая их с договором и требованиями'], ['устраняются ошибки, которые были обнаружены в процессе <br> контроля и оценки']]; //Ответы в правильном порядке
+const anwserArr = [['во время разработки и после нее создается документация и руководства пользователя'], ['процесс включает работу с компонентами программного обеспечения и управление их версиями'], ['необходим для проверки соответствия программного продукта требованиям разработки и стандартам'], ['позволяет независимо оценить проект, документацию и различные отчеты, сравнивая их с договором и требованиями'], ['устраняются ошибки, которые были обнаружены в процессе контроля и оценки']]; //Ответы в правильном порядке
 const text = ["Документирование", "Управление конфигурацией", "Обеспечение качества", "Аудит", "Разрешение проблем"]
 
 const list = document.getElementById('list');
@@ -9,10 +9,9 @@ function reloadWindow(){
     window.location.reload();
 }
 
-
-
 let numberOfQuestion = 10; 
 let numberOfQuestionSum = 10;
+let numberOfEOM = 2;
 
 let textOfQuestionPlace = document.querySelector('#question_number_1')
 
@@ -36,25 +35,28 @@ let stepPlaceDescription = document.querySelector('.number_description');
 stepPlaceDescription.innerHTML = numberOfQuestion + '/' + numberOfQuestionSum;
 
 
-
-
 createList2()
 
 function createList2() {
+    let iii = 0;
     [...anwserArr]
     .map(a => ({ value: a, sort: Math.random() }))
     .sort((a, b) => a.sort - b.sort)
     .map(a => a.value)
     .forEach((item, index) => {
-        const listItem = document.createElement('li');
+        let div = document.createElement("div");
+        div.setAttribute("class", "number");
+        div.innerHTML = `${text[index]}<img src="./content/marker_blue.png" alt="1">`;
+        document.getElementsByClassName("numbers")[0].appendChild(div)
 
-        listItem.setAttribute('id', index);
-        listItem.innerHTML = `
-    <div class="number">${text[index]} <img src="./content/marker_blue.png" alt="1"></div>
-    <div class="item" draggable="true">${item} </div>
-  `;
+        const listItem = document.createElement('div');
+        listItem.setAttribute("id", `${index}`)
+
+        // <div class="number">${text[index]} <img src="./content/marker_blue.png" alt="1"></div>
+        listItem.innerHTML = ` <div class="item">${item} </div> `;
         listItems.push(listItem);
         list.appendChild(listItem);
+        iii++;
     });
 
     addEventListeners();
@@ -91,32 +93,57 @@ function swapItems(fromIndex, toIndex) {
     listItems[toIndex].appendChild(itemOne);
 }
 
-let checkAnwserButton = document.querySelector('#check_button_1')
-let reloadButton = document.querySelector('#check_button_2')
-let nextButton = document.querySelector('#check_button_3')
-checkAnwserButton.classList.remove('disabled_button')
-reloadButton.classList.add('disabled_button')
-nextButton.classList.add('disabled_button')
 
-
-localStorage.setItem('answer_' + numberOfQuestion, JSON.stringify({questionPlace: true}));
 function checkAnwser5() {
-    checkAnwserButton.classList.add('disabled_button')
-    reloadButton.classList.remove('disabled_button')
-    nextButton.classList.remove('disabled_button')
+
+    let nxt = document.getElementById('check_button_3')
+    nxt.removeAttribute('disabled')
+    nxt.classList.remove('blocked')
+    
+    if (numberOfQuestion !== numberOfQuestionSum){
+        nxt.setAttribute('onclick',`location.href='../javascript_quiz_app_${numberOfQuestion+1}/index.html'`)
+    } else {
+        nxt.setAttribute('onclick',`location.href='../javascript_result_page/index.html'`)
+    }
+
+    let rightcheck = 1
     listItems.forEach((item, index) => {
+
         const itemName = item.querySelector('.item').innerText.trim()
         
         if (itemName !== anwserArr[index].join(',')) {
             item.classList.add('incorrect')
+            if(numberOfEOM !== 3){
+                reloadButton.classList.remove('disabled_button')
+            }
+            checkAnwserButton.classList.add('disabled_button')
+            rightcheck=0
             localStorage.setItem('answer_' + numberOfQuestion, JSON.stringify({questionPlace: false}));
+            let nxt = document.getElementById('check_button_3')
+            nxt.removeAttribute('disabled')
+            nxt.classList.remove('blocked')
+            nextButton.classList.remove('disabled_button')
 
         } else {
-            
-            item.classList.remove('incorrect')
+            checkAnwserButton.classList.add('disabled_button')
+            nextButton.classList.remove('disabled_button')
             item.classList.add('correct')
         }
+        
     });
+    if (rightcheck == 1){
+        localStorage.setItem('answer_' + numberOfQuestion, JSON.stringify({questionPlace: true}));
+        let nxt = document.getElementById('check_button_3')
+        nxt.removeAttribute('disabled')
+        nxt.classList.remove('blocked')
+        if (numberOfQuestion !== numberOfQuestionSum){
+            nxt.setAttribute('onclick',`location.href='../javascript_quiz_app_${numberOfQuestion+1}/index.html'`)
+        } else {
+            nxt.setAttribute('onclick',`location.href='../javascript_result_page/index.html'`)
+        }
+        document.getElementById('checkAns').setAttribute('disabled',true)
+        document.getElementById('checkAns').classList.add('blocked')
+    }
 }
 
 function addEventListeners() {
@@ -124,16 +151,24 @@ function addEventListeners() {
     const dragListItems = document.querySelectorAll('.list li');
 
     draggables.forEach((draggable) => {
-        draggable.addEventListener('dragstart', dragStart);
+        // draggable.addEventListener('dragstart', dragStart);
     });
 
     dragListItems.forEach((item) => {
-        item.addEventListener('dragover', dragOver);
-        item.addEventListener('drop', dragDrop);
-        item.addEventListener('dragenter', dragEnter);
-        item.addEventListener('dragleave', dragLeave);
+        // item.addEventListener('dragover', dragOver);
+        // item.addEventListener('drop', dragDrop);
+        // item.addEventListener('dragenter', dragEnter);
+        // item.addEventListener('dragleave', dragLeave);
     });
 }
+
+let checkAnwserButton = document.querySelector('#check_button_1')
+let reloadButton = document.querySelector('#check_button_2')
+let nextButton = document.querySelector('#check_button_3')
+let backwardButton = document.querySelector('#check_button_0')
+checkAnwserButton.classList.remove('disabled_button')
+reloadButton.classList.add('disabled_button')
+nextButton.classList.add('disabled_button')
 
 let openPopUpButton = document.querySelector('#open_popup_button')
 let closePopUpButton = document.querySelector('#close_popup_button_1')
@@ -147,3 +182,5 @@ openPopUpButton.addEventListener('click', function(){
 closePopUpButton.addEventListener('click', function(){
     popUpWindow.classList.add('close')
 })
+
+backwardButton.setAttribute('onclick',`location.href='../javascript_quiz_app_${numberOfQuestion-1}/index.html'`)
